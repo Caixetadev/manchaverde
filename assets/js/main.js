0b7da9c094695f774f2b30000eac2c5edf94f143
Rafael Caixeta <caixetadev@gmail.com>
2021-09-07 01:43:38 -0300

    ♻

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,23 @@ function typeWriter(e) {
 
 
 typeWriter(animationText)
+
+// Hamburguer
+
+const btnMobile = document.getElementById('btn-mobile')
+const closeMenu = document.getElementById('close')
+const navMenu = document.getElementById('nav')
+const closeButtons = document.querySelectorAll('#close')
+
+function toggleMenu() {
+  navMenu.classList.toggle('active')
+}
+
+function closeMenuMobile() {
+  navMenu.classList.remove('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touch', toggleMenu)
+closeMenu.addEventListener('click', closeMenuMobile)
+closeButtons.forEach(link => link.addEventListener('click', closeMenuMobile))
