1e20cfb14f9e0ed6e6f54c52309df3a0565137e4
Rafael Caixeta <caixetadev@gmail.com>
2021-09-07 16:40:56 -0300

    historia

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,20 +1,4 @@
-const animationText = document.querySelector('.animation')
-
-function typeWriter(e) {
-  const textoArray = e.innerHTML.split('')
-  e.innerHTML = ''
-  textoArray.forEach((letra, i) => {
-    setTimeout(() => {
-      e.innerHTML += letra
-    }, 75 * i)
-  })
-}
-
-
-typeWriter(animationText)
-
 // Hamburguer
-
 const btnMobile = document.getElementById('btn-mobile')
 const closeMenu = document.getElementById('close')
 const navMenu = document.getElementById('nav')
